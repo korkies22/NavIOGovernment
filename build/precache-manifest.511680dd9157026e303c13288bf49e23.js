abba441e95519d10f841f99cd555a273d4772705
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-09-17 15:35:59 -0500

    Production ready

---
+++ build/precache-manifest.511680dd9157026e303c13288bf49e23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c863052bfd4e768281221026e6f004a0",
+    "url": "/NavIOGovernment/index.html"
+  },
+  {
+    "revision": "eb68a64fbac175f92110",
+    "url": "/NavIOGovernment/static/css/main.41d84de1.chunk.css"
+  },
+  {
+    "revision": "c510af4097a6a53a5554",
+    "url": "/NavIOGovernment/static/js/2.f6ea9bcf.chunk.js"
+  },
+  {
+    "revision": "eb68a64fbac175f92110",
+    "url": "/NavIOGovernment/static/js/main.a610fdcb.chunk.js"
+  },
+  {
+    "revision": "960f766d1b065212a4fd",
+    "url": "/NavIOGovernment/static/js/runtime~main.169bddc2.js"
+  }
+]);
